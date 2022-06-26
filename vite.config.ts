dc61d04ca0d8fabd65740531142f950cd0e04c25
impossible98 <christoleavenow@outlook.com>
2022-06-26 08:13:07 +0800

    feat: init project

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import progress from 'vite-plugin-progress';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +18,7 @@ export default defineConfig({
     },
   },
   plugins: [
+    progress(),
     vue(),
   ],
   resolve: {
