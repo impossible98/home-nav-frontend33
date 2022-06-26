a2787f8c529848340b9190e522acb05719c86fdf
impossible98 <christoleavenow@outlook.com>
2022-06-26 09:36:03 +0800

    feat: init project

--- vite.config.ts
+++ vite.config.ts
@@ -13,9 +13,9 @@ export default defineConfig({
         manualChunks(id: string) {
           if (id.includes('node_modules')) {
             if (id.includes('node_modules/.pnpm/')) {
-              return id.split('node_modules/.pnpm/')[1].split('/')[0];
+              return id.toString().split('node_modules/.pnpm/')[1].split('/')[0].toString();
             } else {
-              return id.split('node_modules/')[1].split('/')[0];
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
             }
           }
         },
@@ -34,17 +34,17 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'android-chrome-192x192.png',
+            src: 'pwa/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'android-chrome-512x512.png',
+            src: 'pwa/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: 'android-chrome-512x512.png',
+            src: 'pwa/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
