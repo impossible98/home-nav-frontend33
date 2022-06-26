2c3c2a78a79e31c61accdb469f1b0cca8e53872b
impossible98 <christoleavenow@outlook.com>
2022-06-26 09:08:37 +0800

    feat: init project

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import progress from 'vite-plugin-progress';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +21,33 @@ export default defineConfig({
   plugins: [
     progress(),
     vue(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Home Nav',
+        short_name: 'Home Nav',
+        description: 'Your home navigation',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
